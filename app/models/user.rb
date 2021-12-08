013886b654429539271c7838b3da9b051d52b643
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-08 15:35:32 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname 
+    validates :last_name      , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Last name is invalid. Input full-width characters."}
+    validates :first_name     , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "First name is invalid. Input full-width characters."}
+    validates :last_name_kana , format: {with: /\A[ァ-ヶ一]+\z/          , message: "Last name kana is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ一]+\z/          , message: "First name kana is invalid. Input full-width katakana characters."}
+    validates :birth_date 
+  end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Password is invalid. Include both letters and numbers"}
 end
