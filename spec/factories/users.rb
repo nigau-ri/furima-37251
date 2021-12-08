129cf798398b32fd92453a30db7b0c3d4b7d3132
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-08 21:26:18 +0900

    gimei のインストール

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :user do
+    gimei = Gimei.name 
+
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {'山田'}
-    first_name            {'貴史'}
-    last_name_kana        {'ヤマダ'}
-    first_name_kana       {'タカシ'}
+    last_name             {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    last_name_kana        {gimei.last.katakana}
+    first_name_kana       {gimei.first.katakana}
     birth_date            {Faker::Date.between(from: '1930-01-01', to: '2010-12-31')}
   end
 end
