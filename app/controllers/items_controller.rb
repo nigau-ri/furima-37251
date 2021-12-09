616978bbbd04419adde51da5556a8bffce0847df
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 16:43:41 +0900

    Item モデルのストロングパラメータの修正
    画像のパラメータが抜けていたため、追加した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
+      binding.pry
       redirect_to root_path
     else
       render :new
@@ -17,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :price, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
 end
