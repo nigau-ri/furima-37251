e813817024210370e5679f24dcca3a7a20acce7a
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 15:29:34 +0900

    ScheduledDelivery モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
+  belongs_to :scheduled_delivery
   has_one_attached :image
 
   with_options presence: true do
