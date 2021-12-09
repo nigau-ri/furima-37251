4d765380b48a1802646d89bbb9a8270c3ca53904
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 09:43:15 +0900

    FactoryBot の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
 FactoryBot.define do
   factory :user do
-    gimei = Gimei.name 
-
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {gimei.last.kanji}
-    first_name            {gimei.first.kanji}
-    last_name_kana        {gimei.last.katakana}
-    first_name_kana       {gimei.first.katakana}
+    last_name             {Gimei.unique.last}
+    first_name            {Gimei.unique.first}
+    last_name_kana        {last_name.katakana}
+    first_name_kana       {first_name.katakana}
     birth_date            {Faker::Date.between(from: '1930-01-01', to: '2010-12-31')}
   end
 end
