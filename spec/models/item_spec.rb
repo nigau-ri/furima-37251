016f4639b702c18d1705c52bf1fe71c8722c9a57
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 21:29:29 +0900

    Item モデルの単体テストコードの作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品登録' do
+    context '出品登録できる場合' do
+      it '適切に入力すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品登録できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'nameが41文字以上では登録できない' do
+        @item.name = Faker::Internet.password(min_length: 41)
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Name is too long (maximum is 40 characters)')
+      end
+      it 'infoが空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'infoが1001文字以上では登録できない' do
+        @item.info = Faker::Internet.password(min_length: 1001)
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Info is too long (maximum is 1000 characters)')
+      end
+      it 'category_idが0では登録できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'sales_status_idが0では登録できない' do
+        @item.sales_status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it 'shipping_fee_status_idが0では登録できない' do
+        @item.shipping_fee_status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
+      it 'prefecture_idが0では登録できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'schedules_delivery_idが0では登録できない' do
+        @item.scheduled_delivery_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+      it 'priceが300未満では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is invalid')
+      end
+      it 'priceが10000000以上では登録できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is invalid')
+      end
+      it 'priceが全角では登録できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is invalid')
+      end
+    end
+  end
 end
