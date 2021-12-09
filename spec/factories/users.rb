b33f7d88b1f8afee092e99536e730bc7e5c0193f
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 11:53:01 +0900

    FactoryBot の修正
    password を英数混合で生成できるように設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name             { Gimei.unique.last }
     first_name            { Gimei.unique.first }
