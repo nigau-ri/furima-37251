8c8e561e8a769a448435b90e3773c7ab4d287d4f
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 15:57:08 +0900

    items コントローラーにcreateアクションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
+  end
 end
