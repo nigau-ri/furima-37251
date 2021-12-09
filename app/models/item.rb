5d273101606529a48e7b6639e609aafd6a7f7397
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 15:55:43 +0900

    Item モデルにモジュールを取り込むための記述を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
-  has_one_attached :image
 
   with_options presence: true do
     validates :name 
