eb91b34857327e6870d3b066796fe3bff2790803
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 17:36:04 +0900

    入力された価格によって、販売手数料や販売利益の表示が変わることを実装

---
+++ app/javascript/price.js
@@ -0,0 +1,12 @@
+function price() {
+  const itemPrice = document.getElementById('item-price');
+  itemPrice.addEventListener('keyup', ()=>{
+    const addTaxPrice = document.getElementById('add-tax-price');
+    const profit = document.getElementById('profit');
+    const commission = parseInt(itemPrice.value * 0.1);
+    addTaxPrice.innerHTML = commission;
+    profit.innerHTML = itemPrice.value - commission;
+  });
+}
+
+window.addEventListener('load', price);
