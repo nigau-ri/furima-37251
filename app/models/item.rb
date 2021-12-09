d6cdfe86e003d800529ada204912f48009c55462
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-09 14:07:37 +0900

    Item モデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name 
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
+    validates :info
+    validates :category_id            , numericality: {other_than: 0, message: "can't be blank"}
+    validates :sales_status_id        , numericality: {other_than: 0, message: "can't be blank"}
+    validates :shipping_fee_status_id , numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id          , numericality: {other_than: 0, message: "can't be blank"}
+    validates :scheduled_delivery_id  , numericality: {other_than: 0, message: "can't be blank"}
+  end
 end
