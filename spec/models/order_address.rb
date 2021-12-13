9cfd5b81a761e839f26f4069a5dae7b54a93d32b
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-14 08:51:58 +0900

    購入機能のテストコード内のsleepの記述を修正

--- spec/models/order_address.rb
+++ spec/models/order_address.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
-    sleep(1)
     item = FactoryBot.create(:item)
-    sleep(2)
-    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
     sleep(1)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
 
   describe 'クレジットカード決済での購入機能' do
