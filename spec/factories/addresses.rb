520aead45df517b1a6c0e11d7f7db9cbe988ae0c
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-13 14:38:58 +0900

    AddressモデルのFactoryBotの作成

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code   {Faker::Number.number(digits: 3).to_s + "-" + Faker::Number.number(digits: 4).to_s}
+    prefecture_id {Faker::Number.between(from: 1, to: 47)}
+    city          {Gimei.unique.city.kanji}
+    addresses     {Gimei.unique.town.kanji}
+    building      {Faker::Lorem.sentence}
+    phone_number  {Faker::PhoneNumber.subscriber_number(length: 11)}
   end
 end
