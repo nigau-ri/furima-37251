8b8c64855416117149cd95922c7bd78db6a304dd
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-13 15:50:51 +0900

    購入エラーメッセージの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,11 +7,11 @@ class OrderAddress
     validates :user_id
     validates :item_id
 
-    validates :postal_code  , format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code  , format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-). Input half-width digit"}
     validates :prefecture_id, numericality:{other_than: 0, message: "can't be blank"}
     validates :city     
     validates :addresses
-    validates :phone_number , format:{with: /\A[0-9]{10,11}\z/, message: "is invalid. Do not include hyphen(-)"}
+    validates :phone_number , format:{with: /\A[0-9]{10,11}\z/, message: "is invalid. Do not include hyphen(-) Input half-width digit"}
   end
 
   def order_save
