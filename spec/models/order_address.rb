2dfddb17d57ced5d9c0d810733860f51ffe3bdcf
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-13 16:29:08 +0900

    商品購入機能のモデルの単体テストコードの作成

---
+++ spec/models/order_address.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+  end
+
+  describe 'クレジットカード決済での購入機能' do
+    context 'クレジットカード決済での購入ができる場合' do
+      it '適切に入力すれば購入できる' do
+        expect(@order_address).to be_valid
+      end
+      it '建物名は空でも購入できる' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+    context 'クレジットカード決済での購入ができない場合' do
+      it '郵便番号が空だと購入できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号はハイフンなしでは購入できない' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it '郵便番号が全角では購入できない' do
+        @order_address.postal_code = '１２３-４５６７'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it '郵便番号は数字以外が含まれていると購入できない' do
+        @order_address.postal_code = '123-456a'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it '都道府県が空では購入できない' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市区町村が空では購入できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空では購入できない' do
+        @order_address.addresses = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Addresses can't be blank")
+      end
+      it '電話番号が空では購入できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンが含まれていると購入できない' do
+        @order_address.postal_code = '090-1234-1234'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it '電話番号が全角では購入できない' do
+        @order_address.postal_code = '０９０１２３４１２３４'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it '電話番号は数字以外が含まれていると購入できない' do
+        @order_address.postal_code = '0901234abcd'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-). Input half-width digit")
+      end
+      it 'userが空では購入できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが空では購入できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'tokenが空では購入できない' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+    end
+  end
+end
+
