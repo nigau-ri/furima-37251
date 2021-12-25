9fc84d4df349bc275089796bc1210d897ed62c52
nigau-ri <akihito.yoshizawa@gmail.com>
2021-12-25 18:12:58 +0900

    個人ページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(current_user.id)
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+    
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_token)
+      @card = customer.cards.first
+    end
   end
 
 end
